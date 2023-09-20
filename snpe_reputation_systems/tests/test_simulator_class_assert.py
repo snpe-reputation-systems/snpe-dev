d11c34c269a293a1b95609b898152439d7620bed
disanchz <diego.sp10@outlook.es>
2023-09-20 18:29:46 +0200

    test_assert

--- snpe_reputation_systems/tests/test_simulator_class_assert.py
+++ snpe_reputation_systems/tests/test_simulator_class_assert.py
@@ -4,14 +4,15 @@ import hypothesis
 import numpy as np
 import pandas as pd
 import pytest
-from hypothesis import given, settings, assume
+from hypothesis import assume, given, settings
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
-from hypothesis.strategies import integers, floats, none, text, tuples
+from hypothesis.strategies import floats, integers, none, text, tuples
 from numpy import float64
 
 from ..snpe_reputation_systems.simulations.simulator_class import (
     BaseSimulator,
+    SingleRhoSimulator,
     DoubleRhoSimulator,
     HerdingSimulator,
     SingleRhoSimulator,
@@ -20,85 +21,99 @@ from ..snpe_reputation_systems.simulations.simulator_class import (
 # class TestBaseSimulator
 #############################################
 
+
 class TestBaseSimulator:
-    
-    def get_base_simulator(review_prior =  np.array([1, 1, 1, 1, 1]),
-                           tendency_to_rate = 0.05,
-                           simulation_type = "timeseries"):
-        
-        '''
-        Returns a functional instance of BaseSimulator to use for the different 
-        tests for its methods. 
+    def get_base_simulator(
+        review_prior=np.array([1, 1, 1, 1, 1]),
+        tendency_to_rate=0.05,
+        simulation_type="timeseries",
+    ):
+        """
+        Returns a functional instance of BaseSimulator to use for the different
+        tests for its methods.
 
         Although in most cases the default values set for `params` would work,
         the option to manually modify these has been considered in case such
-        flexibility is necesary later in the testing design and implementation 
+        flexibility is necesary later in the testing design and implementation
         process.
-        '''
+        """
 
         params = {
-        "review_prior": review_prior,
-        "tendency_to_rate": tendency_to_rate,
-        "simulation_type": simulation_type,
+            "review_prior": review_prior,
+            "tendency_to_rate": tendency_to_rate,
+            "simulation_type": simulation_type,
         }
 
         return BaseSimulator(params)
-    
-    @settings(max_examples=10)
-    @given(arrays(int, 5, elements=integers(min_value=0, max_value=100)),
-           arrays(arrays(dtype=int, shape=(integers(1, 10),), elements=integers(min_value=0, max_value=100)),
-           text(min_size=3, max_size=15)))
-    
-    def test___init__(array_int5, array_not5, random_string):
 
-        '''
+    @settings(max_examples=10)
+    @given(
+        arrays(int, 5, elements=integers(min_value=0, max_value=100)),
+        arrays(
+            arrays(
+                dtype=int,
+                shape=(integers(1, 10),),
+                elements=integers(min_value=0, max_value=100),
+            ),
+            text(min_size=3, max_size=15),
+        ),
+    )
+    def test___init__(array_int5, array_not5, random_string):
+        """
         Testing builder method by providing it with innapropriate paramerters
-        according to the former "assert"cases provided for BaseSimulator 
+        according to the former "assert"cases provided for BaseSimulator
         in simulator_class.py
-        '''
+        """
 
         # Hypothesis rule so array_not5 cannot take the "correct" shape (5,)
         assume(array_not5.shape != (5,))
 
-        #Testing correct cases
+        # Testing correct cases
 
         assert isinstance(TestBaseSimulator.get_base_simulator(), BaseSimulator)
 
-        assert isinstance(TestBaseSimulator.get_base_simulator(review_prior = array_int5), 
-                          BaseSimulator)
-        
-        assert isinstance(TestBaseSimulator.get_base_simulator(simulation_type = "histogram"), 
-                          BaseSimulator)
+        assert isinstance(
+            TestBaseSimulator.get_base_simulator(review_prior=array_int5), BaseSimulator
+        )
 
-        #Testing incorrect shape of "review_prior"
+        assert isinstance(
+            TestBaseSimulator.get_base_simulator(simulation_type="histogram"),
+            BaseSimulator,
+        )
+
+        # Testing incorrect shape of "review_prior"
 
         with pytest.raises(
-        ValueError,
-        match="Prior Dirichlet distribution of simulated reviews needs to have 5 parameters",
+            ValueError,
+            match="Prior Dirichlet distribution of simulated reviews needs to have 5 parameters",
         ):
+            TestBaseSimulator.get_base_simulator(review_prior=array_not5)
 
-            TestBaseSimulator.get_base_simulator(review_prior =  array_not5)
+        # Testing incorrect values for "simulation type"
 
-        #Testing incorrect values for "simulation type"   
-         
         with pytest.raises(
-        ValueError, match="Can only simulate review histogram or timeseries"
+            ValueError, match="Can only simulate review histogram or timeseries"
         ):
-             TestBaseSimulator.get_base_simulator(simulation_type =  random_string)
-
+            TestBaseSimulator.get_base_simulator(simulation_type=random_string)
 
     @settings(max_examples=10)
-    @given(arrays(dtype=int, shape=(integers(1, 10),), elements=integers(min_value=0, max_value=100)),
-           arrays(dtype=int, shape=5, elements=integers(min_value=0, max_value=100)),
-           arrays(int, 0))
-
-    def test_convolve_prior_with_existing_reviews(array_not5, array_int5, empty_arr, none_value):
-
-        '''
+    @given(
+        arrays(
+            dtype=int,
+            shape=(integers(1, 10),),
+            elements=integers(min_value=0, max_value=100),
+        ),
+        arrays(dtype=int, shape=5, elements=integers(min_value=0, max_value=100)),
+        arrays(int, 0),
+    )
+    def test_convolve_prior_with_existing_reviews(
+        array_not5, array_int5, empty_arr, none_value
+    ):
+        """
         Testing "convolve_prior_with_existing_reviews"
         according to the former "assert"cases provided for this
         BaseSimulator method in simulator_class.py
-        '''
+        """
 
         # Hypothesis rule so array_not5 cannot take the "correct" shape (5,)
         assume(array_not5.shape != (5,))
@@ -110,7 +125,7 @@ class TestBaseSimulator:
         result = base_simulator.convolve_prior_with_existing_reviews(array_int5)
 
         assert np.array_equal(result, np.ones(5) + array_int5)
-        
+
         assert isinstance(result, np.ndarray)
 
         # Testing incorrect cases (1)
@@ -120,4 +135,3 @@ class TestBaseSimulator:
         # Testing  incorrect cases (2)
         with pytest.raises(ValueError):
             base_simulator.convolve_prior_with_existing_reviews(empty_arr)
-
